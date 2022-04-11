b410ec364de35ac72aa46e085be7aee7a684c9f7
Vlasta Vesely <vlastavesely@protonmail.ch>
2022-04-11 14:35:01 +0200

    make PrivateMethod derived from AcquireMethod

---
+++ private-method.cpp
@@ -0,0 +1,2 @@
+#include <iostream>
+#include "private-method.h"
---
+++ private-method.h
@@ -0,0 +1,9 @@
+#ifndef __PRIVATE_METHOD_H
+#define __PRIVATE_METHOD_H
+
+#include "acquire-method.h"
+
+class PrivateMethod : public AcquireMethod {
+};
+
+#endif /* __PRIVATE_METHOD_H */
